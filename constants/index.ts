4fa49dc413ac81aaa26146b5f00ccd7fd7939f93
Gerald <geraldpng@gmail.com>
2022-05-09 16:03:56 +0800

    Remove unused env var

--- constants/index.ts
+++ constants/index.ts
@@ -1,5 +1,4 @@
 export const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
-export const MAP_STYLE = process.env.NEXT_PUBLIC_MAP_STYLE;
 
 export const LOCAL_STORAGE: Record<string, string> = {
   GAME_STATE: 'game_state',
